1feac2f9a0ad5fecd333fc867f302e034078dab7
itc-s23029 <s23029@std.it-college.ac.jp>
2024-08-02 01:40:50 +0000

    第４８回問２３

---
+++ d48Q23kadai.java
@@ -0,0 +1,16 @@
+// s23029
+// 第４８回問２３
+
+class d48Q23kadai {
+	public static void main(String[] args) {
+		int i = 1;
+		for (;;) {
+			i = i +3;
+			if(i < 10) {
+			} else {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+}
