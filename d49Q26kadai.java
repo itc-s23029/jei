02498da8c5238b07a065fe671a9338e5f39dd1cd
itc-s23029 <s23029@std.it-college.ac.jp>
2024-08-02 02:22:31 +0000

    第４９回問２６

---
+++ d49Q26kadai.java
@@ -0,0 +1,13 @@
+// s23029
+// 第４９回問２６
+
+class d49Q26kadai {
+	public static void main(String[] args) {
+		d49Q26kadai odj = new d49Q26kadai();
+		odj.show( 5.5 );
+	}
+
+	void show(double n) {
+		System.out.println("Value is" + n);
+	}
+}
