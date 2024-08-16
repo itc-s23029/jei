d3dfc0caf52b5beb21e928e9e958fd67ee35b24a
it-s23029 <s23029@std.it-college.ac.jp>
2024-08-16 10:07:36 +0900

    第50回問26追加

--- d50Q26kadai.java
+++ d50Q26kadai.java
@@ -1,15 +1,18 @@
-// s23029 d50Q26kadai.java
+// s23029
 // 列挙型の課題
 // Q26をアレンジして、列挙型で定義したConditionの要素すべて
 // を出力する課題です
 // その場合、Condition.values().lengthで要素の数を取得できる
 // そして、 Condition.values()[i]で要素の値を取得できます
 
-enum Condition {ROAD, MUD, SNOW}
+enum Condition {
+	ROAD, MUD, SNOW
+}
 
 class d50Q26kadai {
-	public static void main(String[] args){
-		Condition c = Condition.SNOW;
-		System.out.print("Condition is" + c);
+	public static void main(String[] args) {
+		for (int i = 0; i < Condition.values().length; i++) {
+			System.out.println("Condition " + i + ": " + Condition.values()[i]);
+		}
 	}
 }
